a0363a4792406f28210854886d75c62f8d067d74
camou92 <cmohamed992@gmail.com>
2023-08-05 02:02:46 +0200

    first commit1

--- src/django_blog/settings.py
+++ src/django_blog/settings.py
@@ -80,13 +80,18 @@ WSGI_APPLICATION = 'django_blog.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'django_blog',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'camou92',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432'
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django_blog',
-        'USER': 'postgres',
-        'PASSWORD': 'camou92',
-        'HOST': 'localhost',
-        'PORT': '5432'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
